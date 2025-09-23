9b2d0c80d1593500d50dd32e624db21566445edb
tom mckenna <thomasmckenna12@gmail.com>
2025-09-23 18:53:10 -0500

    installed node, express, mongoose

--- movie-streamer/app/signup.jsx
+++ movie-streamer/app/signup.jsx
@@ -1,9 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 
 const SignUp = () => {
+    const [ email, setUserEmail ] = useState('')
+    const [ password, setUserPassword ] = useState('')
+
+// take username 
+// take passowrd  
+
+
+
     return (
         <View>
             <Text>Sign up</Text>
+            <TextInput
+                onChangeText={setUserEmail}
+            > Email </TextInput>
+            <TextInput
+                onChangeText={setUserPassword}
+            > Password </TextInput>
         </View>
     )
 }
@@ -11,3 +25,5 @@ const SignUp = () => {
 export default SignUp;
 
 const styles = StyleSheet.create({})
+
+
